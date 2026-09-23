aa87d63fb8708281a7b5e2a0cb40d9f2acc490c9
mmove3080-svg <mmove3080@gmail.com>
2026-09-23 13:16:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Only public app assets enter this cache. IndexedDB is never cleared here. */
-const VERSION = 'loft-public-v9';
+const VERSION = 'loft-public-v10';
 const SHELL = [
   './',
   './cloud.js',
